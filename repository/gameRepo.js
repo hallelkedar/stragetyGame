491c8d82a5c9fb9419d22b429997871d982324b1
Hallel <hallelkedar000@gmail.com>
2026-08-23 16:22:23 +0300

    Added map json and repo for initial territtories, fix game repo

--- repository/gameRepo.js
+++ repository/gameRepo.js
@@ -1,11 +1,13 @@
-import db from "../db/dbConnection.js";
+import { ObjectId } from "mongodb";
+import {connectToDB} from "../db/dbConnection.js";
 
+const db = await connectToDB()
 const collection = db.collection("games");
 
 export default {
-  createGame: async (name, teritories) => {
+  createGame: async (playerName, teritories) => {
     const result = await collection.insertOne({
-      name,
+      playerName,
       teritories,
       round: 1,
       phase: "reinforce",
@@ -15,7 +17,8 @@ export default {
    return result.insertedId
   },
   getGameById: async (gameId) => {
-    return await collection.findOne({_id: gameId})
+    
+    return await collection.findOne({_id: new ObjectId(gameId)})
   },
   updateGame: async (gameId, gameData) => {
     const result = await collection.updateOne({_id: gameId}, gameData)
--- repository/mapRepo.js
+++ repository/mapRepo.js
@@ -1,2 +1,22 @@
-import db from "../db/dbConnection.js";
+import {connectToDB} from "../db/dbConnection.js";
+import fs from 'fs/promises'
+
+const db = await connectToDB()
+const collection = db.collection("map");
+
+export default {
+    getMap: async () => {
+        return await collection.find().toArray()
+    },
+    createMap: async (map) => {
+        if (!await collection.find()) {
+            await collection.insertMany(map)
+            return true
+        }
+        return null
+    },
+    getMapJSON: async () => {
+        return await fs.readFile("./repository/ter.json", "utf-8")
+    }
+}
 
