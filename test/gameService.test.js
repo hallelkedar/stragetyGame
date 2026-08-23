19f57394dcc1c4ecb4a9bd1a0d7cd6f51545563b
Hallel <hallelkedar000@gmail.com>
2026-08-23 16:26:20 +0300

    Added test skeleton for 4 checks situations

---
+++ test/gameService.test.js
@@ -0,0 +1,18 @@
+import {describe, it} from "node:test";
+import assert from "node:assert";
+
+describe("game business logic tests", () => {
+    it("should add 3 soldiers to territory 2 (computer is prottecting his HQ", async () => {
+
+    });
+
+    it("should return victory to computer bc he wil winning attack on jerusalem", async () => {
+
+    });
+
+    it("should only do a reinforce and not attack", async () => {
+        
+    });
+
+    it("should move ")
+})
