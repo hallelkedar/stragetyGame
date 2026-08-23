2da26c503e299cf7ce44458b639686c0661f42bf
Hallel <hallelkedar000@gmail.com>
2026-08-23 16:27:18 +0300

    Added express server and env exemple

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import gameControllers from './controllers/gameControllers.js';
+import errorHandler from './middlewares/errorHandler.js';
+import logger from './middlewares/logger.js';
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors());
+app.use(express.json())
+
+app.use(logger)
+
+app.post('/games', gameControllers.newGameCtrl)
+app.get('/games/:id', gameControllers.getGameCtrl)
+app.post('/games/:id/reinforce', gameControllers.reinforceTerCtrl)
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
