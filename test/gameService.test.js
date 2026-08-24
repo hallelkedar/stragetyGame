c242603320a616c674aae9a57212a1757ccf91ba
Hallel <hallelkedar000@gmail.com>
2026-08-24 15:56:15 +0300

    Added some basic tests for reinforce, attack, move to simulate calls

--- test/gameService.test.js
+++ test/gameService.test.js
@@ -59,28 +59,103 @@ describe("game service function test", () => {
       });
       const service = gameService(mockRepo, {});
 
-      assert.rejects(service.reinforce(1, 1), "territory is not valid to reinforce")
+      await assert.rejects(service.reinforce(1, 1), (err) => {
+        assert.strictEqual(err.statusCode, 400);
+        assert.strictEqual(err.message, "territory is not valid to reinforce");
+        return true;
+      });
     });
 
     it("should throw error - territory id is not a number", async () => {
-      const gameRepo = createMockRepo()
-      const service = gameService(gameRepo, {})
+      const gameRepo = createMockRepo();
+      const service = gameService(gameRepo, {});
 
-      assert.rejects(service.reinforce(1, "e"))
+      await assert.rejects(service.reinforce(1, "e"), (err) => {
+        assert.strictEqual(err.statusCode, 400);
+        assert.strictEqual(
+          err.message,
+          "territoryId must be a positive number",
+        );
+        return true;
+      });
     });
   });
 
   describe("check for attack phase", () => {
     it("should attack territory successfully", async () => {
-      
+      const mockRepo = createMockRepo({
+        phase: "attack",
+        territories: [
+          {
+            id: 1,
+            neighbors: [2, 3],
+            distanceFromComputerHQ: 1,
+            distanceFromPlayerHQ: 6,
+            owner: "player",
+          },
+          {
+            id: 2,
+            neighbors: [1, 3],
+            distanceFromComputerHQ: 1,
+            distanceFromPlayerHQ: 6,
+            owner: "computer",
+          },
+        ],
+      });
+      const sevrice = gameService(mockRepo, {});
+
+      const result = await sevrice.attack(1, 1, 2, 2);
+      assert.ok(result.playerEvent);
     });
 
-    it("should throw error - target is not in neihbors", async () => {});
+    it("should throw error - target not found", async () => {
+      const mockRepo = createMockRepo({ phase: "attack" });
+      const service = gameService(mockRepo, {});
+
+      await assert.rejects(service.attack(1, 1, 8, 3), (err) => {
+        assert.strictEqual(err.statusCode, 400);
+        return true;
+      });
+    });
   });
 
   describe("check for move phase", () => {
-    it("should move soldiers successfully", async () => {});
+    it("should move soldiers successfully", async () => {
+      const mockRepo = createMockRepo({
+        phase: "move",
+        territories: [
+          {
+            id: 1,
+            neighbors: [2, 3],
+            distanceFromComputerHQ: 1,
+            distanceFromPlayerHQ: 6,
+            owner: "player",
+          },
+          {
+            id: 2,
+            neighbors: [1, 3],
+            distanceFromComputerHQ: 1,
+            distanceFromPlayerHQ: 6,
+            owner: "player",
+          },
+        ],
+      });
+      const service = gameService(mockRepo, {});
 
-    it("should throw error - source and target are the same", async () => {});
+      const result = await service.move(1, 1, 2, 1);
+      assert.ok(result.playerEvent);
+    });
+
+    it("should throw error - source and target are the same", async () => {
+      const mockRepo = createMockRepo({
+        phase: "move",
+      });
+      const service = gameService(mockRepo, {})
+      
+      await assert.rejects(service.move(1, 2, 2, 2), (e) => {
+        assert.strictEqual(e.statusCode, 400)
+        return true
+      })
+    });
   });
 });
