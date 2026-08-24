0472a0a57c504133ba2c4c80da4f0b25f113f53c
Hallel <hallelkedar000@gmail.com>
2026-08-24 15:13:04 +0300

    Added some basic test for user service

--- test/gameService.test.js
+++ test/gameService.test.js
@@ -1,92 +1,86 @@
-import fs from "fs/promises";
-import { describe, it } from "node:test";
+import { describe, it, mock } from "node:test";
 import assert from "node:assert";
-import {
-  computerAttack,
-  computerMove,
-  computerReinforce,
-  runComputerTurn,
-} from "../service/computerAlgo.js";
+import gameService from "../service/gameService.js";
 
-const getCleanTerritories = async () => {
-  const mapFile = await fs.readFile("./repository/ter.json", "utf-8");
-  const ters = JSON.parse(mapFile);
-  return ters.map((ter) => {
-    return {
-      ...ter,
-      owner: ter.startOwner,
-      soldiers: ter.headquarters ? 8 : 4,
-    };
-  });
+let game;
+
+const createMockRepo = (gameDetails = {}) => {
+  const game = {
+    _id: { toString: () => 1 },
+    phase: "reinforce",
+    status: "playing",
+    winner: null,
+    territories: [
+      {
+        id: 1,
+        neighbors: [2, 3],
+        distanceFromComputerHQ: 1,
+        distanceFromPlayerHQ: 6,
+        owner: "player",
+      },
+      {
+        id: 2,
+        neighbors: [1, 3],
+        distanceFromComputerHQ: 1,
+        distanceFromPlayerHQ: 6,
+        owner: "computer",
+      },
+    ],
+    ...gameDetails,
+  };
+  return {
+    getGameById: mock.fn(async (id) => game),
+    updateGame: mock.fn(async () => null),
+  };
 };
 
-const findTerritory = (territories, id) =>
-  territories.find((ter) => ter.id === id);
+describe("game service function test", () => {
+  describe("check for reinforce phase", () => {
+    it("should add 3 soldiers to correct selected territory", async () => {
+      const mockRepo = createMockRepo();
+      const service = gameService(mockRepo, {});
 
-describe("game business logic tests", () => {
-  it("should add 3 soldiers to territory 2 (computer is prottecting his HQ)", async () => {
-    const map = await getCleanTerritories();
+      const result = await service.reinforce(1, 1);
 
-    const tsidon = findTerritory(map, 4);
-    tsidon.owner = "player";
-    tsidon.soldiers = 10;
+      assert.strictEqual(result.playerEvent.soldiersAdded, 3);
+    });
 
-    const result = computerReinforce(map);
+    it("should throw error - reinforce on non player owner ter", async () => {
+      const mockRepo = createMockRepo({
+        territories: [
+          {
+            id: 1,
+            neighbors: [2, 3],
+            distanceFromComputerHQ: 1,
+            distanceFromPlayerHQ: 6,
+            owner: "computer",
+          },
+        ],
+      });
+      const service = gameService(mockRepo, {});
 
-    assert.ok(result, "should successfully return computerEvent");
-    assert.strictEqual(result.territoryId, 2);
-    assert.strictEqual(result.soldiersAdded, 3);
+      assert.rejects(service.reinforce(1, 1), "territory is not valid to reinforce")
+    });
+
+    it("should throw error - territory id is not a number", async () => {
+      const gameRepo = createMockRepo()
+      const service = gameService(gameRepo, {})
+
+      assert.rejects(service.reinforce(1, "e"))
+    });
   });
 
-  it("should return victory to computer bc computer wil winning attack on jerusalem", async () => {
-    const map = await getCleanTerritories();
+  describe("check for attack phase", () => {
+    it("should attack territory successfully", async () => {
+      
+    });
 
-    const afula = findTerritory(map, 16);
-    afula.owner = "computer";
-    afula.soldiers = 20;
-
-    const jerusalem = findTerritory(map, 17);
-    jerusalem.soldiers = 3;
-
-    const result = computerAttack(map);
-
-    assert.ok(result, "actually attack and not decide to skip");
-    assert.ok(
-      result.headquartersCaptured,
-      "a hq is captured and computer is winning",
-    );
+    it("should throw error - target is not in neihbors", async () => {});
   });
 
-  it("should only do a reinforce and not attack", async () => {
-    const map = await getCleanTerritories();
+  describe("check for move phase", () => {
+    it("should move soldiers successfully", async () => {});
 
-    map.forEach(
-      (ter) => (ter.soldiers = ter.owner === "player" ? 20 : ter.soldiers),
-    );
-
-    const reinforceResult = computerReinforce(map);
-    const attackResult = computerAttack(map);
-
-    assert.ok(reinforceResult);
-    assert.ok(!attackResult);
-  });
-
-  it("should move soldiers only from back (not border ter)", async () => {
-    const map = await getCleanTerritories();
-
-    const computerTer = map.filter((ter) => ter.owner === "computer");
-    const nonBorderTers = computerTer.filter((ter) =>
-      ter.neighbors.filter((id) => findTerritory(map, id).owner !== "player"),
-    );
-    const nonBorderIds = nonBorderTers.map((t) => t.id);
-
-    const game = {
-      territories: map,
-    };
-    const result = runComputerTurn(game);
-    const moveFrom = result[2].fromId;
-
-    assert.ok(result[2]);
-    assert.ok(nonBorderIds.includes(moveFrom));
+    it("should throw error - source and target are the same", async () => {});
   });
 });
